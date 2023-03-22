de38d559fc53b8e2f478a239def8458c54d74a7e
almol <Alexander.Mol@UGent.be>
2023-03-22 19:45:31 +0100

    update

--- scripts/typedscript.js
+++ scripts/typedscript.js
@@ -73,12 +73,39 @@ new Swiper('.myDesign-slider', {
     }
 });
 
-$('a[href^="#"]').on('click', function (event) {
-    var target = $(this.getAttribute('href'));
-    if (target.length) {
-        event.preventDefault();
-        $('html, body').stop().animate({
-            scrollTop: target.offset().top
-        }, 1000);
+/**
+ * Porfolio isotope and filter
+ */
+window.addEventListener('load', () => {
+    let portfolioContainer = select('.portfolio-container');
+    if (portfolioContainer) {
+        let portfolioIsotope = new Isotope(portfolioContainer, {
+            itemSelector: '.portfolio-item'
+        });
+
+        let portfolioFilters = select('#portfolio-flters li', true);
+
+        on('click', '#portfolio-flters li', function (e) {
+            e.preventDefault();
+            portfolioFilters.forEach(function (el) {
+                el.classList.remove('filter-active');
+            });
+            this.classList.add('filter-active');
+
+            portfolioIsotope.arrange({
+                filter: this.getAttribute('data-filter')
+            });
+            portfolioIsotope.on('arrangeComplete', function () {
+                AOS.refresh()
+            });
+        }, true);
     }
-}); 
+
+});
+
+/**
+ * Initiate portfolio lightbox 
+ */
+const portfolioLightbox = GLightbox({
+    selector: '.portfolio-lightbox'
+});
