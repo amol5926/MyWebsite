4d555636672a9551b30a42efbf1161921f47b230
amol5926 <125655669+amol5926@users.noreply.github.com>
2023-06-29 00:24:09 +0200

    Update index.js
    comments op code

--- scripts/index.js
+++ scripts/index.js
@@ -1,49 +1,49 @@
-var typed = new Typed(".overMij", {
-    strings: [, "Industrial designer", "Engineering student", "Photographer", "Prop builder"],
-    loop: true,
-    typeSpeed: 80,
-    backSpeed: 20,
-    backDelay: 2000
+var typed = new Typed(".overMij", {    //.overMij selector voor vaar typed effect wordt toegepast
+    strings: [, "Industrial designer", "Engineering student", "Photographer", "Prop builder"], //array van strings die uitgetypt zullen worden
+    loop: true, //effect blijft loopen
+    typeSpeed: 80, //elk karakter getypt met 80 ms delay
+    backSpeed: 20, //karakters verwijdert met snelheid van 20 ms delay
+    backDelay: 2000 //2000 ms voordat getypte string terug verwijderd wordt
 });
 
 
 
 
 //veranderen van kleur navbar bij scrollen
-window.addEventListener('scroll', function () {
-    var navbar = document.querySelector('.navbar');
+window.addEventListener('scroll', function () { //event listener voor scrollen
+    var navbar = document.querySelector('.navbar'); //selecteren van elementen
     var targetElement = document.querySelector('.hero-title');
-    var navbarHeight = navbar.getBoundingClientRect().height;
-    var targetPosition = targetElement.getBoundingClientRect().top + window.scrollY;
-    var threshold = targetPosition - navbarHeight - 50;
-    if (window.scrollY > threshold) {
-        navbar.classList.add('navbar-scrolled');
+    var navbarHeight = navbar.getBoundingClientRect().height; //bepalen hoogte navbar
+    var targetPosition = targetElement.getBoundingClientRect().top + window.scrollY;//bepalen van positie
+    var threshold = targetPosition - navbarHeight - 50; // drempelwaarde voor het wijzigen van stijl
+    if (window.scrollY > threshold) {     //kijken of huidige scrollpositie de drempelwaarde overschrijdt
+        navbar.classList.add('navbar-scrolled');   //nieuwe klasse toevoegen aan navbar
     } else {
-        navbar.classList.remove('navbar-scrolled');
+        navbar.classList.remove('navbar-scrolled');  //verwijderen van de klasse navbar-scrolled van navbar
     }
 });
 
 //knop om terug naar boven te gaan (kan waarschijnlijk zonder js, nog bekijken)
-let mybutton = document.getElementById("btn-back-to-top");
+let mybutton = document.getElementById("btn-back-to-top"); //knop selecteren
 
-window.onscroll = function () {
+window.onscroll = function () { //event handler doe wordt opgeroepen wanneer scroll-evenement optreedt
     scrollFunction();
 };
 
 function scrollFunction() {
-    if (
-        document.body.scrollTop > 20 ||
+    if (    //controleren van de scrollpositie oude en nieuwe browsers
+        document.body.scrollTop > 20 || 
         document.documentElement.scrollTop > 20
     ) {
-        mybutton.style.display = "block";
+        mybutton.style.display = "block"; //zichtbaar maken
     } else {
-        mybutton.style.display = "none";
+        mybutton.style.display = "none";    //onzichtbaar maken
     }
 }
 // Het scrollen zelf
-mybutton.addEventListener("click", backToTop);
+mybutton.addEventListener("click", backToTop); //event listener voor knop
 
-function backToTop() {
+function backToTop() { // terug naar boven scrollen
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
@@ -55,12 +55,12 @@ function backToTop() {
     /**
      * Easy selector helper function
      */
-    const select = (el, all = false) => {
-        el = el.trim()
+    const select = (el, all = false) => { //functie "select" neemt elementselector en optionele parameter
+        el = el.trim() //spaties verwijderen van element selector
         if (all) {
-            return [...document.querySelectorAll(el)]
+            return [...document.querySelectorAll(el)] // retourneert een array van alle elementen die overeenkomen met de selector
         } else {
-            return document.querySelector(el)
+            return document.querySelector(el) //retourneert eerste overeenkomstige element
         }
     }
 
@@ -68,12 +68,12 @@ function backToTop() {
      * Easy event listener function
      */
     const on = (type, el, listener, all = false) => {
-        let selectEl = select(el, all)
+        let selectEl = select(el, all) //roept de 'select' functie aan om het gewenste element te selecteren
         if (selectEl) {
             if (all) {
-                selectEl.forEach(e => e.addEventListener(type, listener))
+                selectEl.forEach(e => e.addEventListener(type, listener)) //voegt event listeners toe aan alle overeenkomstige elementen
             } else {
-                selectEl.addEventListener(type, listener)
+                selectEl.addEventListener(type, listener) // voegtr event listener toe aan eerste overeenkomstige element
             }
         }
     }
@@ -86,26 +86,26 @@ function backToTop() {
      * Porfolio isotope and filter
      */
     window.addEventListener('load', () => {
-        let portfolioContainer = select('.portfolio-container');
-        if (portfolioContainer) {
-            let portfolioIsotope = new Isotope(portfolioContainer, {
-                itemSelector: '.portfolio-item'
+        let portfolioContainer = select('.portfolio-container'); //select functie om element met klasse "portfolio-container" te selecteren--> toegewezen aan variabele "porfoliocontainer"
+        if (portfolioContainer) {   //als portfoliocontainer bestaat wordt de rest van de code uitgevoerd
+            let portfolioIsotope = new Isotope(portfolioContainer, { //aanmaak nieuw isotope-object en koppeling aan geselecteerde 'portfolioContainer'-element
+                itemSelector: '.portfolio-item' //isotope zal elementen met klasse portfolio-item beschouwen als items in het portfolio
             });
 
-            let portfolioFilters = select('#portfolio-flters li', true);
-
-            on('click', '#portfolio-flters li', function (e) {
-                e.preventDefault();
-                portfolioFilters.forEach(function (el) {
-                    el.classList.remove('filter-active');
+            let portfolioFilters = select('#portfolio-flters li', true); //selecteert alle lijst elementen binnen het element met id "portfolio-filter"--> alle overeenkomstige elementen moeten worden geselecteerd
+//Deze variabelen worden toegewezen aan variabele 'portfolio-filters'
+            on('click', '#portfolio-flters li', function (e) { //klik gebeurtenis hanteren op lijst elementen binnen #portfolio filters, als geklikt wordt de functie uitgevoerd
+                e.preventDefault(); // Voorkom de standaardgedraging van de klikgebeurtenis
+                portfolioFilters.forEach(function (el) { // Itereer over elk element in "portfolioFilters"
+                    el.classList.remove('filter-active'); // Verwijder de klasse 'filter-active' van elk element
                 });
-                this.classList.add('filter-active');
+                this.classList.add('filter-active'); // Voeg de klasse 'filter-active' toe aan het geselecteerde li-element
 
-                portfolioIsotope.arrange({
+                portfolioIsotope.arrange({ 
                     filter: this.getAttribute('data-filter')
-                });
-                portfolioIsotope.on('arrangeComplete', function () {
-                    AOS.refresh()
+                });  // Pas de filterinstellingen van Isotope aan op basis van de geselecteerde filter
+                portfolioIsotope.on('arrangeComplete', function () { // Wanneer het rangschikken van de items in Isotope is voltooid
+                    AOS.refresh() // Werk de AOS-bibliotheek bij met de nieuwe indeling van de items
                 });
             }, true);
         }
@@ -117,27 +117,27 @@ function backToTop() {
 
 })()
 
-const toggleSwitch = document.querySelector('#dark-mode-toggle');
+const toggleSwitch = document.querySelector('#dark-mode-toggle'); // Selecteer het element met het id 'dark-mode-toggle' en wijs het toe aan de variabele 'toggleSwitch'
 
-function switchTheme(e) {
+function switchTheme(e) {   // Definieer een functie genaamd 'switchTheme' die wordt opgeroepen wanneer er een wijziging plaatsvindt in het toggle-element
     if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark'); // save the user's preference to localStorage
+        document.documentElement.setAttribute('data-theme', 'dark'); // Stel het attribuut 'data-theme' van het document in op 'dark' om de donkere modus in te schakelen
+        localStorage.setItem('theme', 'dark'); // Sla de voorkeur van de gebruiker op als 'dark' in de localStorage
     } else {
-        document.documentElement.setAttribute('data-theme', 'light');
-        localStorage.setItem('theme', 'light'); // save the user's preference to localStorage
+        document.documentElement.setAttribute('data-theme', 'light'); // Stel het attribuut 'data-theme' van het document in op 'light' om de lichte modus in te schakelen
+        localStorage.setItem('theme', 'light'); // Sla de voorkeur van de gebruiker op als 'dark' in de localStorage
     }
 }
 
-// add a listener to the toggle switch
+// Voeg een event listener toe aan het toggle-element
 toggleSwitch.addEventListener('change', switchTheme, false);
 
-// check the user's preference from localStorage and set the initial theme accordingly
-const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+// Controleer de voorkeur van de gebruiker in de localStorage en stel het thema dienovereenkomstig in
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null; // Haal de huidige themavoorkeur op uit de localStorage, of wijs null toe als er geen voorkeur is opgeslagen
 if (currentTheme) {
-    document.documentElement.setAttribute('data-theme', currentTheme);
+    document.documentElement.setAttribute('data-theme', currentTheme); // Stel het attribuut 'data-theme' van het document in op de huidige themavoorkeur
     if (currentTheme === 'dark') {
-        toggleSwitch.checked = true;
+        toggleSwitch.checked = true; // Controleer de toggle-switch als het huidige thema 'dark' is
     }
 }
 
